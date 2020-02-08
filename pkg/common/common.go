01ca0e0670f82cd25dec8b0b2a64bbe588572f9e
Christopher Nuland <cnuland@localhost.localdomain>
2020-02-08 18:28:50 -0500

    clean house

--- pkg/common/common.go
+++
@@ -1,9 +0,0 @@
-package common
-
-const (
-	ImageSigningTypeAnnotation  = "image-signing"
-	ImageScanningTypeAnnotation = "image-scanning"
-	ControllerAgentName         = "image-scan-sign-controller"
-	CopOwnerAnnotation          = "cop.redhat.com/owner"
-	CopTypeAnnotation           = "cop.redhat.com/type"
-)
